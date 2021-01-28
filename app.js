0e8482bcbbe5a36b31413ce6dd8f5ce84cd1934a
jashimgit <jashim.ncn@gmail.com>
2021-01-28 18:50:14 +0600

    totalCoverPrice function added

--- app.js
+++ app.js
@@ -11,6 +11,39 @@ document.querySelector('.removePhoneQtyBtn').addEventListener('click', function 
 
 })
 
+
+// cover section
+document.querySelector('.add-cover-btn').addEventListener('click', function () {
+    totalCoverPrice(true);
+})
+document.querySelector('.remove-cover-btn').addEventListener('click', function () {
+    totalCoverPrice(false)
+})
+
+
+function totalCoverPrice(isIncrease) {
+    // get cover quantity value from input box
+    let coverQty = parseInt(document.querySelector('.cover-qty').value);
+    // increase new Cover quantity  by 1 per click 
+    let newCoverQty = 0;
+    //   set condition
+    if (isIncrease == true) {
+        newCoverQty = coverQty + 1;
+    }
+    if (isIncrease == false && newCoverQty > 0) {
+        newCoverQty = coverQty - 1;
+    }
+
+    // set total new cover quantity after increase to input box
+    let totalCoverQty = document.querySelector('.cover-qty').value = newCoverQty;
+    // count total cover price as per total input quantity
+    let totalCoverPrice = parseInt(document.querySelector('#total-cover-price').innerText);
+    // count total price for total cover counted
+    totalCoverPrice = totalCoverQty * 59;
+    // set total price to price area
+    document.querySelector('#total-cover-price').innerText = totalCoverPrice;
+}
+
 function totalPhonePrice(isIncrease) {
     // get current input quantity for phone input box
     let phoneQty = parseInt(document.querySelector('.add-product-qty').value);
@@ -32,32 +65,3 @@ function totalPhonePrice(isIncrease) {
     document.querySelector('#iphone-price').innerText = totalPhonePrice;
 }
 
-// cover section
-document.querySelector('.add-cover-btn').addEventListener('click', function () {
-    // get cover quantity value from input box
-    let coverQty = parseInt(document.querySelector('.cover-qty').value);
-    // increase new Cover quantity  by 1 per click 
-    let newCoverQty = coverQty + 1;
-    // set total new cover quantity after increase to input box
-    let totalCoverQty = document.querySelector('.cover-qty').value = newCoverQty;
-    // count total cover price as per total input quantity
-    let totalCoverPrice = parseInt(document.querySelector('#total-cover-price').innerText);
-    // count total price for total cover counted
-    totalCoverPrice = totalCoverQty * 59;
-    // set total price to price area
-    document.querySelector('#total-cover-price').innerText = totalCoverPrice;
-})
-document.querySelector('.remove-cover-btn').addEventListener('click', function () {
-    // get cover quntity value from input box
-    let coverQty = parseInt(document.querySelector('.cover-qty').value);
-    // decrease quantity by 1 per click
-    let newCoverQty = coverQty - 1;
-    // set new cover count quantity
-    let totalCoverQty = document.querySelector('.cover-qty').value = newCoverQty;
-    // set total cover price to a variable
-    let totalCoverPrice = parseInt(document.querySelector('#total-cover-price').innerText);
-    // count total cover price for selected quantity
-    totalCoverPrice = totalCoverQty * 59;
-    // set total price to price area
-    document.querySelector('#total-cover-price').innerText = totalCoverPrice;
-})
