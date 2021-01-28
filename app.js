26613f225dbf8dd4b0c6770430dbf1d9772e34ec
jashimgit <jashim.ncn@gmail.com>
2021-01-28 18:58:31 +0600

    total phone price working in console log

--- app.js
+++ app.js
@@ -1,44 +1,16 @@
 
 //  get add phone quantity button
-const addPhoneQtyBtn = document.querySelector('.addPhoneQtyBtn');
-const addCoverBtn = document.querySelector('.add-cover-btn');
 
-addCoverBtn.addEventListener('click', function () {
-    let phonePerCoverPrice = 59;
-    // get input value
-    let coverQtyInput = parseFloat(document.querySelector('.add-cover-qty').value);
-    let totalCoverQty = document.querySelector('.add-cover-qty').value = coverQtyInput + 1;
-    // console.log(parseFloat(totalCoverQty));
-    // get total-cover-price 
-    var totalCoverPrice = parseFloat(document.querySelector('#total-cover-price').innerText);
+document.querySelector('.addPhoneQtyBtn').addEventListener('click', function () {
 
-    totalCoverPrice = phonePerCoverPrice * totalCoverQty;
-    // console.log(totalCoverPrice);
-    document.querySelector('#total-cover-price').innerText = totalCoverPrice;
+    // get current input quantity for phone input box
+    let phoneQty = parseInt(document.querySelector('.add-product-qty').value);
+    // total phone quanity 
+    let totalPhoneQty = document.querySelector('.add-product-qty').value = phoneQty + 1;
+    let totalPhonePrice = parseInt(document.querySelector('#iphone-price').innerText);
+    // total Phone Price 
+    totalPhonePrice = totalPhoneQty * 1219;
+
+    // set total price in the innerText 
+    // document.querySelector('iphone-price').innerText = totalPhonePrice;
 })
-
-
-
-
-
-
-addPhoneQtyBtn.addEventListener('click', function () {
-    let phonePrice = 1219;
-    // get add-product quantity value
-    const productQty = parseInt(document.querySelector('.add-product-qty').value);
-
-    const totalPhoneQty = document.querySelector('.add-product-qty').value = productQty + 1;
-    // get phone price 
-    const price = parseInt(document.querySelector('#iphone-price').innerText);
-    // count total phone price 
-    document.querySelector('#iphone-price').innerText = totalPhoneQty * phonePrice;
-
-})
-
-
-
-
-
-function subTotal(phonePrice, phoneCoverPrice) {
-
-}
