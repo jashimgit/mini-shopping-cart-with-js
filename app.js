ee9a95cca028938456bdf757a1040dd58900ea48
jashimgit <jashim.ncn@gmail.com>
2021-01-28 17:22:26 +0600

    totalPhonePrice function applied to both increase phone button and decrease phone button

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
 document.querySelector('.addPhoneQtyBtn').addEventListener('click', function () {
 
-    totalPhonePrice();
+    totalPhonePrice(true);
     // // get current input quantity for phone input box
     // let phoneQty = parseInt(document.querySelector('.add-product-qty').value);
     // // total phone quanity 
@@ -17,21 +17,29 @@ document.querySelector('.addPhoneQtyBtn').addEventListener('click', function () 
 })
 
 document.querySelector('.removePhoneQtyBtn').addEventListener('click', function () {
-    // get current input qunantity
-    // console.log('remove btn cliked');
-    let phoneQty = parseInt(document.querySelector('.add-product-qty').value);
-    let newPhoneQty = phoneQty - 1; 
-    let totalPhoneQty = document.querySelector('.add-product-qty').value = newPhoneQty;
-    // total Phone price 
-    totalPhonePrice = totalPhoneQty * 1219;
-    // set total price in the innerText 
-    document.querySelector('#iphone-price').innerText = totalPhonePrice;
+    totalPhonePrice(false)
+    // // get current input qunantity
+    // // console.log('remove btn cliked');
+    // let phoneQty = parseInt(document.querySelector('.add-product-qty').value);
+    // let newPhoneQty = phoneQty - 1; 
+    // let totalPhoneQty = document.querySelector('.add-product-qty').value = newPhoneQty;
+    // // total Phone price 
+    // totalPhonePrice = totalPhoneQty * 1219;
+    // // set total price in the innerText 
+    // document.querySelector('#iphone-price').innerText = totalPhonePrice;
 })
 
-function totalPhonePrice() {
+function totalPhonePrice(isIncrease) {
     // get current input quantity for phone input box
     let phoneQty = parseInt(document.querySelector('.add-product-qty').value);
-    let newPhoneQty = phoneQty + 1;
+    let newPhoneQty = phoneQty;
+
+    if (isIncrease == true) {
+        newPhoneQty = newPhoneQty + 1;
+    }
+    if (isIncrease == false) {
+        newPhoneQty = newPhoneQty - 1;
+    }
     // total phone quanity 
     let totalPhoneQty = document.querySelector('.add-product-qty').value = newPhoneQty;
     let totalPhonePrice = parseInt(document.querySelector('#iphone-price').innerText);
